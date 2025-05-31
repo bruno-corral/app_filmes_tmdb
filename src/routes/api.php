eff02891134562b6ec25251c3ea173f9fe98fe3e
bruno <brunoalcorral@gmail.com>
2025-05-31 12:41:59 -0300

    fix:Alterar rota para showFavoriteMovies
    - Alterado a rota para o método showFavoriteMovies

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('movies', MoviesController::class);
 Route::get('/movies/search/{movie}', [MoviesController::class, 'show']);
-Route::get('/movies/search/favorite-movies', [MoviesController::class, 'showFavoriteMovies']);
+Route::get('/movies/search/favorite/movies', [MoviesController::class, 'showFavoriteMovies']);
 Route::post('/movies/add-movie', [MoviesController::class, 'storeFavoriteMovie']);
 
 Route::get('/genres', [MoviesController::class, 'genres']);
